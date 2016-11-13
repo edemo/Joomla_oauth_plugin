912966e9c452f4dacdf916bfaef54e87acea1281
Fogler Tibor (Utopszkij) <tibor.fogler@gmail.com>
2016-11-13 14:34:13 +0100

    Update ada_objTest.php

--- tests/ada_objTest.php
+++ tests/ada_objTest.php
@@ -5,20 +5,21 @@ require_once "adalogin/site/models/ada_obj.php";
 class adaloginTest extends PHPUnit_Framework_TestCase
 {
     public function test_getLoginURI_correctly()  {
-		$dbResults[0] = JSON_decode('{
-		"id":1, 
-		"ADA_AUTH_URI":"https://adatom.hu/ada/v1/oauth2/auth", 
-		"ADA_USER_URI":"https://adatom.hu/ada/v1/users/me", 
-		"ADA_TOKEN_URI":"https://adatom.hu/ada/v1/oauth2/token", 
-		"appkey":"APP_ID_COMES_HERE", 
-		"secret":"secret_comes_here", 
-		"joomla_psw":"joomla_psw_comes_here"
-		}');
+	global $dbResults;
+	$dbResults[0] = JSON_decode('{
+	"id":1, 
+	"ADA_AUTH_URI":"https://adatom.hu/ada/v1/oauth2/auth", 
+	"ADA_USER_URI":"https://adatom.hu/ada/v1/users/me", 
+	"ADA_TOKEN_URI":"https://adatom.hu/ada/v1/oauth2/token", 
+	"appkey":"APP_ID_COMES_HERE", 
+	"secret":"secret_comes_here", 
+	"joomla_psw":"joomla_psw_comes_here"
+	}');
         $ada = new adaloginModelAda_obj();
         $resp = $ada->getloginFormURI();
         $this->assertEquals(
-            "https://adatom.hu/ada/v1/oauth2/auth?response_type=code&client_id=APP_ID_COMES_HERE&redirect_uri=https%3A%2F%2Flocalhost%2Fcomponents%2Fcom_adalogin%2Findex.php",
-            $resp);
+           "https://adatom.hu/ada/v1/oauth2/auth?response_type=code&client_id=APP_ID_COMES_HERE&redirect_uri=https%3A%2F%2Flocalhost%2Fcomponents%2Fcom_adalogin%2Findex.php",
+           $resp);
     }
 }
 ?>
