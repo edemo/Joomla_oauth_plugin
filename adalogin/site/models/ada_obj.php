795fc3cf35a28538d7d159ec1d831d5b9deed3aa
utopszkij <tibor.fogler@gmail.com>
2017-01-12 12:18:56 +0100

    ADA login joomla popup ablakba #27

--- adalogin/site/models/ada_obj.php
+++ adalogin/site/models/ada_obj.php
@@ -8,28 +8,6 @@
 */ 
 
 
-
-class JoomlaInterface {
-	public function remoteCall($url,$method,$data,$extraHeader='') {
-		$result = '';
-		if ($extraHeader != '') {
-			$extraHeader .= "\r\n";
-		}	
-		$options = array(
-			'http' => array(
-				'header'  => "Content-type: application/x-www-form-urlencoded\r\n".$extraHeader,
-				'method'=> $method,
-				'content' => http_build_query($data)
-		    )
-		);
-		$context  = stream_context_create($options);
-		return file_get_contents($url, false, $context);
-	}
-}
-
-global $theJoomlaInterface;
-$theJoomlaInterface = new JoomlaInterface();
-
 class AdaloginModelAda_obj {
 	public $joomla_psw;
 
@@ -40,16 +18,8 @@ class AdaloginModelAda_obj {
 	protected $secret; 
 	protected $myURI; 
 	protected $home;
-	protected $interface;
 
-    function __construct($iface = false) {
-        global $theJoomlaInterface;
-		if($iface)
-		{
-			$this->interface = $iface;
-		} else {
-			$this->interface = $theJoomlaInterface;
-        }
+    function __construct() {
 		$db = JFactory::getDBO();
 		$db->setQuery('select * from #__adalogin order by id limit 1');
 		$res = $db->loadObject();
@@ -59,17 +29,19 @@ class AdaloginModelAda_obj {
 		$this->myURI = 'https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
 		$i = strpos($this->myURI,'?');
 		if ($i > 0) $this->myURI = substr($this->myURI,0,$i);
-		$this->home = str_replace('/components/com_adalogin','','https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+		// https is wrong $this->home = str_replace('/components/com_adalogin','','https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+		$this->home = str_replace('/components/com_adalogin','','http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
 		$i = strpos($this->home,'?');
 		if ($i > 0) $this->home = substr($this->home,0,$i);
-		$this->home = str_replace('/ssologin','',$this->home); // for old ssologin interface enviroment
-		$this->home = str_replace('/adalogin','',$this->home); // for old adalogin interface enviroment
+		//$this->home = str_replace('/ssologin','',$this->home); // for old ssologin interface enviroment
+		//$this->home = str_replace('/adalogin','',$this->home); // for old adalogin interface enviroment
     }
 
 	/**
 	* redirect ADA server, get loginform. Call this method only in joomla component.controller
 	*/
 	public function getLoginURI($redi='') {
+		
 	  $redirectURI = JURI::base().'components/com_adalogin/index.php';	
 	  // $redirectURI = JURI::base().'ssologin/index.php';	uncomment this line in old ssologin enviroment
 	  // $redirectURI = JURI::base().'adalogin/index.php';	uncomment this line in old adalogin enviroment
@@ -88,7 +60,25 @@ class AdaloginModelAda_obj {
 	  * @return string
 	*/
 	public function remoteCall($url,$method,$data,$extraHeader='') {
-        return $this->interface->remoteCall($url,$method,$data,$extraHeader);
+		$result = '';
+		if ($extraHeader != '') {
+			$extraHeader .= "\r\n";
+		}	
+		$options = array(
+			'http' => array(
+				'header'  => "Content-type: application/x-www-form-urlencoded\r\n".$extraHeader,
+				'method'=> $method,
+				'content' => http_build_query($data)
+		    )
+		);
+		if (_UNITTEST == 1) {
+		  global $testData;	
+		  $result = $testData->getRemoteResult();
+		} else {
+		  $context  = stream_context_create($options);
+		  $result = file_get_contents($url, false, $context);
+		}
+		return $result;
 	}
 	
 	/**
@@ -149,7 +139,8 @@ class AdaloginModelAda_obj {
 	public function callback() {
 		$input = JFactory::getApplication()->input;
 		$db = JFactory::getDBO();
-		$userData = null;
+		$session = JFactory::getSession();
+
 		$token = $this->getADAtoken($input->get('code'));
 		// get user data
 		if (isset($token->access_token)) {
@@ -159,13 +150,12 @@ class AdaloginModelAda_obj {
 			$userData->assurances = JSON_encode($userData->assurances);
 		}
 		//$this->home = str_replace('https:','http:',$this->home); //uncomment if https: not supported
-		$userData = JSON_decode('{"userid":30,"email":"30@adatom.hu"}');	
-	
+		
 		if (isset($userData->userid)) {
 			echo '<html>
 			<body>
 			<h2>ADA login client ...</h2>
-			<form name="form1" method="post" action='.$db->quote($this->home).'>
+			<form name="form1" method="post" action='.$db->quote($this->home).' target="_top">
 			<input type="hidden" name="option" value="com_adalogin" />
 			<input type="hidden" name="task" value="dologin" />
 			<input type="hidden" name="Itemid" value="0" />
@@ -176,7 +166,15 @@ class AdaloginModelAda_obj {
 			'.JHTML::_( 'form.token' ).'
 			</form>
 			<script type="text/javascript">
-			  document.forms.form1.submit();
+			  //document.forms.form1.submit();
+			  window.opener.location="'.$this->home.'?option=com_adalogin"+
+			  "&task=dologin&Itemid=0"+
+			  "&'.$session->getFormToken().'=1"+
+			  "&adaid='.$userData->userid.'"+
+			  "&adaemail='.urlencode($userData->email).'"+
+			  "&assurance='.urlencode($userData->assurances).'"+
+			  "&redi='.$input->get("redi","","string").'";
+			  window.close();
 			</script>
 			</body>
 			</html>
--- adalogin/site/models/adalogin.php
+++ adalogin/site/models/adalogin.php
@@ -5,8 +5,6 @@
 * @subpackage 	Models
 * @copyright	Copyright (C) 2016, Fogler Tibor. All rights reserved.
 * @license #GNU/GPL
-*
-* 2016.12.23 V4.01 extradata kezelés támogatása, újabb joomla psw kezelési eljárás, "assurance_xxxxx" usergroups kezelés
 */
 // no direct access
 defined('_JEXEC') or die('Restricted access');
@@ -16,8 +14,6 @@ defined('_JEXEC') or die('Restricted access');
  */
  
 jimport( 'joomla.application.component.model' ); 
-if (file_exists(JPATH_ADMINISTRATOR.'/components/com_users/models/group.php'))
-   include_once JPATH_ADMINISTRATOR.'/components/com_users/models/group.php';
 
 /**
 * this component not use model
@@ -48,7 +44,7 @@ class AdaloginModelAdalogin extends JModelLegacy  {
 	  if ($res) {
 		$result = JFactory::getUser($res->id);
 	  } else {
-		$db->setQuery('select * from #__users where params like "{%\"ADA\":\"'.$adaid.'\"%"');
+		$db->setQuery('select * from #__users where params like "{%\"ADA\":\"'.$db->quote($adaid).'\"%"');
 		$res = $db->loadObject();
 		if ($res) {
 		  $result = JFactory::getUser($res->id);
@@ -82,105 +78,26 @@ class AdaloginModelAdalogin extends JModelLegacy  {
   }	
 
   /**
-  * get user group by group title
-  * @param string title
-  * @return integer group.id
-  */
-  public function getGroupByTitle($group_title) {
-	$groupModel = new UsersModelGroup();
-	$db = JFactory::getDBO();
-	$db->setQuery('select * from #__usergroups where title='.$db->quote($group_title).' limit 1');
-	$group = $db->loadObject();
-	if ($group == false) {
-	  // create new group parent: 2 (registered)	
-	  $groupData = array(
-      'title' => $group_title,
-      'parent_id' => 2,  
-      'id' => 0);
-	  if ($groupModel->save($groupData))
-	    return $this->getGroupByTitle($group_title);  
-	  else
-		return false;  
-	} else {
-	  return $group->id;	
-	}  
-  }
-
-  /**
-  * load group record from database
-  * @param integer group.id
-  * @return recordObject
-  */
-	public function getGroup($groupId) {
-		$groupModel = new UsersModelGroup();
-		return $groupModel->getItem($groupId);	  
-	}  
-  
-	/**
-	* set assurances into usergoups
-	* @param JUser
-	* @param string 
-	* @return void
-	*/
-	public function	setUserAssurances(&$user, $assurances) {
-		// delete old assurences from usergroups
-		foreach ($user->groups as $groupId) {
-			$group = $this->getGroup($groupId);
-			if (substr($group->title,0,10) == 'assurance_') unset($user->groups[$groupId]);
-		}
-		// set new assurances into usergroup
-		$assurancesArray = explode(',', $assurances);  // assurance is string.
-		foreach ($assurancesArray as $ass) {
-			$ass = str_replace('[','',$ass);
-			$ass = str_replace(']','',$ass);
-			$groupId = $this->getGroupByTitle('assurance_'.$ass);
-			$user->groups[$groupId] = $groupId;
-		}
-		$user->setParam('ASSURANCE',$assurances);
-		$user->save();
-	}	
-
-  
-  /**
   * create joomla account
   * @param string adaid
   * @param string nick
   * @param string adaemail
   * @param JSON_encoded assurance
-  * @param string extradata    (2016.12.15 update)
   * @return boolean and set errorMsg  
   */
-  public function save($adaid, $nick, $adaemail, $assurances, $extrafields=array()) {
+  public function save($adaid, $nick, $adaemail, $assurance) {
 	$result = true;
-	$db = JFactory::getDBO();
-	$params = new stdClass();
-	$params->ADA = $adaid;
-	$params->ASSURANCE = $assurances;
-	$psw = md5($adaid.$this->PSW);
 	$data = array(
           "name"=>$nick,
           "username"=>$nick,
-          "password"=>$psw,
-          "password2"=>$psw,
-		  "params"=>$params,
+          "password"=>$this->PSW,
+          "password2"=>$this->PSW,
+		  "params"=>JSON_decode('{"ADA":"'.$adaid.'","ASSURANCE":"'.$assurance.'"}'),
 		  "activation"=>"",
           "email"=>$adaemail,
           "block"=>0,
-          "groups"=>array("2")
+          "groups"=>array("1","2")
     );
-	// set groups from assurances
-	foreach ($assurances as $assurance) {
-		$groupId = $this->getGroupByTitle('assurance_'.$assurance);
-		$data->groups[$groupId] = $groupId;
-	}
-	if (isset($extrafields['name'])) {
-		if ($extrafields['name'] != '')
-			$data['name'] = $extrafields['name'];
-	}	
-	if (isset($extrafields['email'])) {
-		if ($extrafields['email'] != '')
-			$data['email'] = $extrafields['email'];
-	}
     $user = new JUser();
     if(!$user->bind($data)) {
 		  $result = false;
@@ -189,17 +106,7 @@ class AdaloginModelAdalogin extends JModelLegacy  {
     if (!$user->save()) {
 		  $result = false;
           $this->setError(JText::_('ADALOGIN_ERROR').' '. $user->getError());
-    } else {
-		// save data to user_profile. (Figyelem a str adatokat "xxx"-ként kell tárolni!
-		$i = 1;
-		foreach ($extrafields as $fn => $fv) {
-			if (($fn != 'name') & ($fn != 'email')) {
-				$db->setQuery('insert into #__user_profiles values ('.$user->id.',"profile.'.$fn.'",'.$db->quote('"'.$fv.'"').','.$i.')');
-				$db->query();
-				$i++;
-			}
-		}
-	}
+    }
 	return $result;	
   }
 
@@ -212,21 +119,11 @@ class AdaloginModelAdalogin extends JModelLegacy  {
   public function loginToJoomla($adaid, $adaemail) {
     $user = $this->getUser($adaid, $adaemail); 
 	$credentials = array();
-	
-	//1. próba az újabb (V4.01) eljárás szerinti psw -al
 	$credentials['username'] = $user->username;
-	$credentials['password'] = md5($adaid.$this->PSW);
+	$credentials['password'] = $this->PSW;
 	$user->id = 0; 
 	$result = JFactory::getApplication()->login($credentials);
-	if ($result == false) {
-		//2. próba a régi psw eljárással
-		$credentials['username'] = $user->username;
-		$credentials['password'] = $this->PSW;
-		$result = JFactory::getApplication()->login($credentials);
-	}
-	if ($result == false) {
-	  $this->setError('Error in Joomla login');
-	}  
+	if ($result == false) $this->setError('Error in Joomla login');
 	return $result;
   }	  
   
